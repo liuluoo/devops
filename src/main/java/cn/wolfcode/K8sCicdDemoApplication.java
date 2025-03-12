88a47291019910161e57b6e97afb8da5a6c0e98b
liulu <liulu@git.cn>
2025-03-13 03:47:46 +0800

    5

--- src/main/java/cn/wolfcode/K8sCicdDemoApplication.java
+++ src/main/java/cn/wolfcode/K8sCicdDemoApplication.java
@@ -3,7 +3,8 @@ package cn.wolfcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootAp
+plication
 public class K8sCicdDemoApplication {
 
     public static void main(String[] args) {
